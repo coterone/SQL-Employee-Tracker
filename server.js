6a75db69c7ce80e526d943f3e25da746fc88f8fb
coterone <137343969+coterone@users.noreply.github.com>
2023-12-18 20:24:34 -0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const db = mysql.createConnection(
     {
       host: 'localhost',
       user: 'root',
-      password: 'Papi$hampo0',
+      password: '',
       database: 'ets_db'
     },
     (err) => {
